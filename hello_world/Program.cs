43d8f77de1b23943b44421e8d1a07c71c8417adb
Juan David <juandruiz298@gmail.com>
2025-08-08 23:43:32 -0500

    string and int, input keyboard

--- hello_world/Program.cs
+++ hello_world/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// declaring a variable and using codesnippets
+
+string name = "Juan";
+
+System.Console.WriteLine(name);
+
+name = "David";
+
+System.Console.WriteLine(name);
+
+System.Console.WriteLine("Introduce the first number: ");
+
+int num1 = int.Parse(Console.ReadLine());
+
+System.Console.WriteLine("Introduce the second number: ");
+
+int num2 = int.Parse(Console.ReadLine());
+
+int result = num1 + num2;
+
+System.Console.WriteLine("The result is: " + result); 
+
+
