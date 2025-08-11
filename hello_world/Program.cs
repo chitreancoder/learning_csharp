8d5170b65072eb84b714b37b8fc85c66b8343821
Juan David <juandruiz298@gmail.com>
2025-08-10 21:32:26 -0500

    logic

--- hello_world/Program.cs
+++ hello_world/Program.cs
@@ -1,14 +1,11 @@
-﻿// Calculator
-Console.WriteLine("Enter the first number: ");
-int firstNum = int.Parse(Console.ReadLine());
-Console.WriteLine("Enter the second number: ");
-int secondNum = int.Parse(Console.ReadLine());
+﻿bool isRaining = true;
+bool hasUmbrella = false;
 
-Console.WriteLine("The sum of the two numbers is: " + (firstNum + secondNum));
-Console.WriteLine("The difference of the two numbers is: " + (firstNum - secondNum));
-Console.WriteLine("The product of the two numbers is: " + (firstNum * secondNum));
-Console.WriteLine("The quotient of the two numbers is: " + (firstNum / secondNum));
-Console.WriteLine("The remainder of the two numbers is: " + (firstNum % secondNum));
-
-
-
+if ((isRaining && hasUmbrella) || !isRaining)
+{
+    Console.WriteLine("I'll be fine");
+}
+else if (isRaining && !hasUmbrella)
+{
+    Console.WriteLine("I'll get wet");
+}
