b96686f00954c373eee37205dfd278823f868bf9
Juan David <juandruiz298@gmail.com>
2025-08-10 22:00:03 -0500

    calculator app

--- hello_world/Program.cs
+++ hello_world/Program.cs
@@ -1,11 +1,39 @@
-﻿bool isRaining = true;
-bool hasUmbrella = false;
+﻿using System;
 
-if ((isRaining && hasUmbrella) || !isRaining)
+namespace Coding.Exercise
 {
-    Console.WriteLine("I'll be fine");
-}
-else if (isRaining && !hasUmbrella)
-{
-    Console.WriteLine("I'll get wet");
+    public class Exercise
+    {
+        public void SimpleCalculator()
+        {
+            Console.WriteLine("Enter the first number:");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the second number:");
+            int num2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Choose an operation: +, -, *, /");
+            string operation = Console.ReadLine();
+            
+            if(operation == "+"){
+                int result = num1 + num2;
+                Console.WriteLine("Result: " + result);
+            }else if (operation == "-"){
+                 int result = num1 - num2;
+                Console.WriteLine("Result: " + result);
+            }else if (operation == "*"){
+                      int result = num1 * num2;
+                Console.WriteLine("Result: " + result);
+            }else if (operation == "/"){
+                if(num2 == 0){
+                       Console.WriteLine("Error: Division by zero is not allowed.");
+                }else {
+                          int result = num1 / num2;
+                Console.WriteLine("Result: " + result);
+                }
+              
+            }else {
+                   Console.WriteLine("Invalid operation. Please choose +, -, *, or /.");
+            }
+            
+        }
+    }
 }
