95793a2b6eb1cc25d0ce9c7265ed7b12a362d914
JD <juandruiz298@gmail.com>
2025-09-03 22:30:55 +0200

    list basics

---
+++ ListApp/Program.cs
@@ -0,0 +1,22 @@
+﻿List<string> colors = new List<string>{"Red", "Blue", "Green", "Blue"};
+
+/*colors.Add("Red");
+colors.Add("Blue");
+colors.Add("Green");
+colors.Add("Blue");*/
+
+colors.Add("Yellow");
+
+bool blueRemoved = colors.Remove("Blue");
+while (blueRemoved)
+{
+    blueRemoved = colors.Remove("Blue");
+}
+Console.WriteLine("Blue removed");
+
+Console.WriteLine("Colors: ");
+
+foreach (string color in colors)
+{
+    Console.WriteLine(color);
+}
