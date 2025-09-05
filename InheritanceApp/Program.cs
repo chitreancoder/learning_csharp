7a7cef5f5720ef51c56021fca21be64abf95980a
JD <juandruiz298@gmail.com>
2025-09-05 10:32:29 +0200

    access levels

--- InheritanceApp/Program.cs
+++ InheritanceApp/Program.cs
@@ -10,14 +10,27 @@ Animal animal = new Animal();
 animal.Eat();
 
 public class Animal {
+
+    public int publicField = 1;
+    protected int protectedField = 2;
+    private int privateField = 3;
     public void Eat() {
         Console.WriteLine("I am eating");
+        Console.WriteLine(publicField);
+        Console.WriteLine(protectedField);
+        Console.WriteLine(privateField);
     }
 }
 
 public class Dog : Animal {
     public void Bark() {
         Console.WriteLine("I am barking");
+    }
+
+    public void AccessFields() {
+        publicField = 4;
+        protectedField = 5;
+        // privateField = 6;
     }
 }
 
