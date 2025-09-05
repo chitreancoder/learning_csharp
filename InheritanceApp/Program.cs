b559608013bd8909df6850c5322b8838d3f5e22f
JD <juandruiz298@gmail.com>
2025-09-05 10:51:17 +0200

    abstraction

--- InheritanceApp/Program.cs
+++ InheritanceApp/Program.cs
@@ -1,55 +1,39 @@
-﻿Animal animal = new Animal();
-animal.Eat();
-animal.MakeSound();
-animal.Sleep();
-
-Dog dog = new Dog();
-dog.Eat();
+﻿Dog dog = new Dog();
 dog.Bark();
-dog.MakeSound();
-dog.Sleep();
+dog.Hunt();
 
 
-public class Animal {
+abstract class Animal
+{
 
-    public int publicField = 1;
-    protected int protectedField = 2;
-    private int privateField = 3;
-    public void Eat() {
-        Console.WriteLine("I am eating");
-    }
-    public virtual void MakeSound() {
-        Console.WriteLine("I am making a sound");
-    }
 
-    public void Sleep(){
-        Console.WriteLine("I am sleeping");
-    }
+    public abstract void Hunt();
 }
-
-public class Dog : Animal {
-    public void Bark() {
+class Dog : Animal
+{
+    public void Bark()
+    {
         Console.WriteLine("I am barking");
     }
 
-    public new void Sleep(){
-        base.Sleep();
-        Console.WriteLine("I am sleeping loudly");
+
+    public override void Hunt()
+    {
+        Console.WriteLine("I am hunting");
     }
 
-    public override void MakeSound() {
-        Console.WriteLine("I am barking");
-    }
 
-    public void AccessFields() {
-        publicField = 4;
-        protectedField = 5;
-        // privateField = 6;
-    }
 }
 
-public class Cat : Animal {
-    public void Meow() {
+class Cat : Animal
+{
+    public void Meow()
+    {
         Console.WriteLine("I am meowing");
+    }
+
+    public override void Hunt()
+    {
+        Console.WriteLine("I am hunting");
     }
 }
