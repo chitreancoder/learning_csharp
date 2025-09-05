1d4f6fa98c94c34d3b1b736efb335263949a91a9
JD <juandruiz298@gmail.com>
2025-09-05 10:24:36 +0200

    inheritance

---
+++ InheritanceApp/Program.cs
@@ -0,0 +1,28 @@
+﻿Cat cat = new Cat();
+cat.Eat();
+cat.Meow();
+
+Dog dog = new Dog();
+dog.Eat();
+dog.Bark();
+
+Animal animal = new Animal();
+animal.Eat();
+
+public class Animal {
+    public void Eat() {
+        Console.WriteLine("I am eating");
+    }
+}
+
+public class Dog : Animal {
+    public void Bark() {
+        Console.WriteLine("I am barking");
+    }
+}
+
+public class Cat : Animal {
+    public void Meow() {
+        Console.WriteLine("I am meowing");
+    }
+}
