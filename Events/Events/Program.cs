1b111635fb83ba7e5f6f6b29af63542d9a17b6fb
JD <juandruiz298@gmail.com>
2025-09-06 19:27:48 +0200

    evens basics

---
+++ Events/Events/Program.cs
@@ -0,0 +1,26 @@
+﻿
+EventPublisher pub = new EventPublisher();
+EventSubscriber sub = new EventSubscriber();
+pub.OnNotify += sub.OnEventRaised;
+pub.RaiseEvent("Testing event");
+
+public delegate void Notify(string msg);
+
+public class EventPublisher
+{
+    public event Notify OnNotify;
+
+    public void RaiseEvent(string msg)
+    {
+        OnNotify?.Invoke(msg); // Invokes event only if there are susbscribers.
+        
+    }
+}
+
+public class EventSubscriber
+{
+    public void OnEventRaised(string msg)
+    {
+        Console.WriteLine("Event received: " + msg);
+    }
+}
