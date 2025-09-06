1848b8c4e1990032c7ccbb6bb99f4895676f7fc4
JD <juandruiz298@gmail.com>
2025-09-06 13:43:17 +0200

    delegates

---
+++ Delegates/Delegates/Program.cs
@@ -0,0 +1,14 @@
+﻿Notify notifyDelegate = ShowMessage;
+
+notifyDelegate("Ciao");
+
+
+
+static void ShowMessage(string message)
+{
+    Console.WriteLine(message);
+}
+
+
+public delegate void Notify(string message);
+
--- EnumsDateTimeGarbage/EnumsDateTimeGarbage/Program.cs
+++ EnumsDateTimeGarbage/EnumsDateTimeGarbage/Program.cs
@@ -1,7 +1,1 @@
 ﻿
-
-DateTime myBirthday =  new DateTime(1998, 08, 29);
-DateTime today = DateTime.Today;
-
-int daysSinceMyBirthday = today.Subtract(myBirthday).Days;
-Console.WriteLine(daysSinceMyBirthday);
