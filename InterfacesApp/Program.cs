6d3fbc16250f497d1448504bea487a7cef31c085
JD <juandruiz298@gmail.com>
2025-09-06 10:35:48 +0200

    base interfaces

---
+++ InterfacesApp/Program.cs
@@ -0,0 +1,43 @@
+﻿Dog dog = new Dog();
+dog.MakeSound();
+dog.Eat("Food");
+
+Cat cat = new Cat();
+cat.MakeSound();
+cat.Eat("Essen");
+
+
+public interface IAnimal
+{
+    void MakeSound();
+
+    void Eat(string food);
+}
+
+public class Dog : IAnimal
+{
+    public void MakeSound()
+    {
+        Console.WriteLine("Dog sound");
+    }
+
+    public void Eat(string food)
+    {
+        Console.WriteLine("Dog food");
+    }
+
+    
+}
+
+public class Cat : IAnimal
+{
+    public void MakeSound()
+    {
+        Console.WriteLine("Cat sound");
+    }
+
+    public void Eat(string food)
+    {
+        Console.WriteLine("Cat food");
+    }
+}
