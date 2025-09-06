16370c358166f51cc2b8fe754da97b4a90b32dfe
JD <juandruiz298@gmail.com>
2025-09-06 11:37:09 +0200

    dependency injcetions

---
+++ DependencyInjectionApp/DependencyInjectionApp/Program.cs
@@ -0,0 +1,62 @@
+﻿Hammer hammer = new Hammer();
+Saw saw = new Saw();
+Builder builder = new Builder(hammer);
+builder.Saw = saw;
+ScrewDriver screwDriver = new ScrewDriver();
+builder.SetScrewDriver(screwDriver);
+builder.Build();
+
+
+public interface ITools
+{
+    void SetScrewDriver(ScrewDriver screwDriver);
+}
+
+
+public class ScrewDriver
+{
+    public void Use()
+    {
+        Console.WriteLine("Screwing");
+    }
+}
+
+public class Hammer
+{
+    public void Use()
+    {
+        Console.WriteLine(("Hammering"));
+    }
+}
+
+public class Saw
+{
+    public void Use()
+    {
+        Console.WriteLine("Sawing");
+    }
+}
+
+public class Builder : ITools
+{
+    private ScrewDriver _screwDriver;
+    private Hammer _hammer;
+    public Saw Saw { get; set; }
+
+    public void SetScrewDriver(ScrewDriver screwDriver)
+    {
+        _screwDriver = screwDriver;
+    }
+
+    public Builder(Hammer hammer)
+    {
+        _hammer = hammer;
+    }
+
+    public void Build()
+    {
+        _hammer.Use();
+        Saw.Use();
+        _screwDriver.Use();
+    }
+}
--- InterfacesApp/Program.cs
+++ InterfacesApp/Program.cs
@@ -1,3 +1,47 @@
 ﻿
-string dirPath = "/Users/kaori/dev/learning_csharp/InterfacesApp/log.txt";
-File.AppendAllText(dirPath, "Hello, World! !");
+ILogger fileLogger = new FileLogger();
+Application app = new Application(fileLogger);
+app.Log();
+
+
+ILogger dbLogger = new DatabaseLogger();
+app = new Application(dbLogger);
+app.Log();
+
+
+public interface ILogger
+{
+    void Log(string message);
+}
+
+public class FileLogger : ILogger
+{
+    public void Log(string message)
+    {
+        Console.WriteLine("Logging to file: " + message);
+    }
+}
+
+public class DatabaseLogger : ILogger
+{
+    public void Log(string message)
+    {
+        Console.WriteLine("Logging to DB: " + message);
+    }
+}
+
+public class Application
+{
+    private readonly ILogger _logger;
+
+    public Application(ILogger logger)
+    {
+        _logger = logger;
+    }
+
+    public void Log()
+    {
+        _logger.Log("Do the work");
+        _logger.Log("Work done");
+    }
+}
